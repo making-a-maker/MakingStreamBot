0e694ae7b3f27d3529a65f42b222385801021199
Aaron <53849714+makeraaron@users.noreply.github.com>
2019-08-10 20:21:14 -0700

    added rainbow

--- command_processor/commander.py
+++ command_processor/commander.py
@@ -63,6 +63,10 @@ class CommandProcessor(threading.Thread):
             if leds_enabled:
                 if cmd in solid.keys():
                     cp.led_process(self.pixels, cmd)
+                if cmd in ["pride", "rainbow"]:
+                    for i in range(len(self.config["num_leds"])):
+                        self.pixels[i] = wheel(i)
+                        self.pixels.show()
 
             with self.tco_lock:
                 self.tco.command_ready_flag.clear()
@@ -72,5 +76,24 @@ class CommandProcessor(threading.Thread):
 # del user
 # colors?
 # listen for sub / raid / follow
-
+def wheel(pos):
+    # Input a value 0 to 255 to get a color value.
+    # The colours are a transition r - g - b - back to r.
+    if pos < 0 or pos > 255:
+        r = g = b = 0
+    elif pos < 85:
+        r = int(pos * 3)
+        g = int(255 - pos*3)
+        b = 0
+    elif pos < 170:
+        pos -= 85
+        r = int(255 - pos*3)
+        g = 0
+        b = int(pos*3)
+    else:
+        pos -= 170
+        r = 0
+        g = int(pos*3)
+        b = int(255 - pos*3)
+    return (r, g, b) if ORDER == neopixel.RGB or ORDER == neopixel.GRB else (r, g, b, 0)
 
