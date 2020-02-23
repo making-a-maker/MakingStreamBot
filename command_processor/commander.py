3214b954d1efb8bb10831d51d3f7ebc1528c0adf
makeraaron <53849714+makeraaron@users.noreply.github.com>
2020-02-22 18:23:25 -0800

    removed waiting for flag to process command

--- command_processor/commander.py
+++ command_processor/commander.py
@@ -61,30 +61,38 @@ class CommandProcessor(threading.Thread):
                 # If queue is not empty, then get command, and proceed to process it
                 if not self.tco.command.empty():
                     self.command = self.tco.command.get()
+                    self.command_ready = True
                     logger.info("Commands remaining = {}".format(self.tco.command.qsize()))
-                # However, if the queue is empty, wait for the event to trigger processing
-                # (trying to maintain responsiveness)
+                # # However, if the queue is empty, wait for the event to trigger processing
+                # # (trying to maintain responsiveness)
+                # else:
+                #     self.command_ready_flag.wait()
+                #     logger.warning("COMMAND EVENT RECEIVED - Processing...")
                 else:
-                    self.command_ready_flag.wait()
-                    logger.warning("COMMAND EVENT RECEIVED - Processing...")
+                    self.command_ready = False
 
-            # Strip off first character of command
-            cmd = self.command[1]
-            logger.info("Processing command: '{}'".format(cmd))
 
-            if leds_enabled:
-                SOLID = utils.load_solid_colors()
-                if cmd in SOLID.keys():
-                    led_process(self.pixels, SOLID[cmd])
-                elif cmd in ["pride", "rainbow"]:
-                    logger.debug("....Rainbow")
-                    for _ in range(5):
-                        self.rainbow_cycle(0.005)
 
-            with self.tco_lock:
-                self.tco.command_ready_flag.clear()
-            
-            time.sleep(self.config["min_command_time"])
+            if self.command_ready:
+                # Strip off first character of command
+                cmd = self.command[1]
+                logger.info("Processing command: '{}'".format(cmd))
+
+                if leds_enabled:
+                    SOLID = utils.load_solid_colors()
+                    if cmd in SOLID.keys():
+                        led_process(self.pixels, SOLID[cmd])
+                    elif cmd in ["pride", "rainbow"]:
+                        logger.debug("....Rainbow")
+                        for _ in range(5):
+                            self.rainbow_cycle(0.005)
+
+                with self.tco_lock:
+                    self.tco.command_ready_flag.clear()
+
+                time.sleep(self.config["min_command_time"])
+            else:
+                time.sleep(0.25)
 
 # Proposed commands
 # add user
